0b0401fc2f0c87525926340e8058e46b0aea50e5
BenasB <29711974+BenasB@users.noreply.github.com>
2022-03-23 00:59:59 +0200

    Added LinearMortgage logic

--- 2/loan-calculator/src/main/java/com/loan/App.java
+++ 2/loan-calculator/src/main/java/com/loan/App.java
@@ -5,16 +5,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-import com.loan.business.AnnuityMortgage;
-import com.loan.dto.NewInputData;
-
 public class App extends Application {
-
-    final NewInputData testInputData = new NewInputData(12000f, 0.005f, 3, 0, NewInputData.MortgageType.Annuity);
 
     @Override
     public void start(Stage stage) throws IOException {
-        new AnnuityMortgage(testInputData);
         new WindowManager(stage, "main", "Mortgage calculator");
     }
 
--- 2/loan-calculator/src/main/java/com/loan/business/LinearMortgage.java
+++ 2/loan-calculator/src/main/java/com/loan/business/LinearMortgage.java
@@ -4,14 +4,26 @@ import com.loan.dto.NewInputData;
 
 public class LinearMortgage extends Mortgage {
 
-  LinearMortgage(NewInputData inputData) {
+  public LinearMortgage(NewInputData inputData) {
     super(inputData);
   }
 
   @Override
   MonthlyPayment[] generateMonthlyPayments(NewInputData inputData) {
-    // TODO Auto-generated method stub
-    return null;
+    int months = inputData.years * 12 + inputData.months;
+    double principalRepayment = inputData.amount / months;
+
+    MonthlyPayment[] result = new MonthlyPayment[months];
+
+    double principal = inputData.amount;
+    for (int month = 1; month <= months; month++) {
+      double interestRepayment = principal * inputData.interest;
+      double paymentAmount = principalRepayment + interestRepayment;
+      result[month - 1] = new MonthlyPayment(month, paymentAmount, principalRepayment, interestRepayment, principal);
+      principal -= principalRepayment;
+    }
+
+    return result;
   }
 
 }
