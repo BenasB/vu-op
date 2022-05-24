432c2f36fdd0e8d8008940d09ce80827899353a4
BenasB <29711974+BenasB@users.noreply.github.com>
2022-05-25 02:04:53 +0300

    Added conditionals

--- 4/core/src/com/sokoban/game/parser/Parser.java
+++ 4/core/src/com/sokoban/game/parser/Parser.java
@@ -32,7 +32,12 @@ public class Parser extends Thread {
         String[] lines = codeInput.split("\n");
 
         for (int linePointer = 0; (linePointer < lines.length) && running.get(); linePointer++) {
-            System.out.println("Line pointer: " + linePointer);
+            if (runner.getIgnoreLines()) {
+                System.out.println("Ignoring line pointer: " + linePointer);
+            } else {
+                System.out.println("Line pointer: " + linePointer);
+            }
+
             linePointer = handleLine(lines[linePointer].trim(), linePointer);
         }
 
@@ -41,7 +46,7 @@ public class Parser extends Thread {
     }
 
     private int handleLine(String line, int linePointer) {
-        if (line.contains("();")) {
+        if (line.contains("();") && !runner.getIgnoreLines()) {
             String functionName = line.substring(0, line.indexOf("();"));
             runner.performParameterlessFunction(functionName);
 
@@ -52,11 +57,15 @@ public class Parser extends Thread {
             }
         } else if (line.contains("(") && line.contains("){")) {
             // Block command
-            String commandName = line.substring(0, line.indexOf("("));
-            String argument = line.substring(line.indexOf("(") + 1, line.indexOf(")"));
+            String commandName = line.substring(0, line.indexOf("(")).trim();
+            String argument = line.substring(line.indexOf("(") + 1, line.indexOf(")")).trim();
             runner.startBlockCommand(commandName, argument, linePointer);
         } else if (line.equals("}")) {
             return runner.endLatestBlockCommand(linePointer);
+        } else if (line.contains("=") && line.contains(";") && !runner.getIgnoreLines()) {
+            String variableName = line.substring(0, line.indexOf("=")).trim();
+            String argument = line.substring(line.indexOf("=") + 1, line.indexOf(";")).trim();
+            runner.assignVariable(variableName, argument);
         }
 
         return linePointer;
--- 4/core/src/com/sokoban/game/parser/Runner.java
+++ 4/core/src/com/sokoban/game/parser/Runner.java
@@ -1,5 +1,6 @@
 package com.sokoban.game.parser;
 
+import java.util.HashMap;
 import java.util.Stack;
 
 import com.sokoban.game.GameManager;
@@ -8,6 +9,8 @@ import com.sokoban.game.Player;
 public class Runner {
     private Player player;
     private Stack<BlockCommand> blockCommandStack = new Stack<>();
+    private HashMap<String, Integer> variableMap = new HashMap<>();
+    private boolean ignoreLines = false;
 
     private class BlockCommand {
         public boolean repeat = false;
@@ -17,6 +20,10 @@ public class Runner {
 
     public Runner() {
         player = GameManager.getInstance().getPlayer();
+    }
+
+    public boolean getIgnoreLines() {
+        return ignoreLines;
     }
 
     public void performParameterlessFunction(String functionName) {
@@ -41,18 +48,66 @@ public class Runner {
     public void startBlockCommand(String commandName, String argument, int linePointer) {
         System.out.println("Starting block command: " + commandName + " with argument: " + argument);
         if (commandName.equals("repeat")) {
-            Integer times = Integer.parseInt(argument);
-            BlockCommand repeatBlock = new BlockCommand();
-            repeatBlock.startingLine = linePointer;
-            repeatBlock.times = times;
-            repeatBlock.repeat = true;
-            blockCommandStack.add(repeatBlock);
+            if (!ignoreLines) {
+                Integer times = tryParseAndSubstituteVariable(argument);
+                BlockCommand repeatBlock = new BlockCommand();
+                repeatBlock.startingLine = linePointer;
+                repeatBlock.times = times;
+                repeatBlock.repeat = true;
+                blockCommandStack.add(repeatBlock);
+            } else {
+                BlockCommand repeatBlock = new BlockCommand();
+                repeatBlock.times = 1;
+                repeatBlock.repeat = true;
+                blockCommandStack.add(repeatBlock);
+            }
+        } else if (commandName.equals("if")) {
+            BlockCommand ifBlock = new BlockCommand();
+            ifBlock.repeat = false;
+            blockCommandStack.add(ifBlock);
+
+            if (ignoreLines)
+                return;
+
+            if (argument.contains("==")) {
+                String[] operands = argument.split("==");
+                int op1 = tryParseAndSubstituteVariable(operands[0].trim());
+                int op2 = tryParseAndSubstituteVariable(operands[1].trim());
+                ignoreLines = op1 == op2;
+            } else if (argument.contains(">=")) {
+                String[] operands = argument.split(">=");
+                int op1 = tryParseAndSubstituteVariable(operands[0].trim());
+                int op2 = tryParseAndSubstituteVariable(operands[1].trim());
+                ignoreLines = op1 >= op2;
+            } else if (argument.contains(">")) {
+                String[] operands = argument.split(">");
+                int op1 = tryParseAndSubstituteVariable(operands[0].trim());
+                int op2 = tryParseAndSubstituteVariable(operands[1].trim());
+                ignoreLines = op1 > op2;
+            } else if (argument.contains("<=")) {
+                String[] operands = argument.split("<=");
+                int op1 = tryParseAndSubstituteVariable(operands[0].trim());
+                int op2 = tryParseAndSubstituteVariable(operands[1].trim());
+                ignoreLines = op1 <= op2;
+            } else if (argument.contains("<")) {
+                String[] operands = argument.split("<");
+                int op1 = tryParseAndSubstituteVariable(operands[0].trim());
+                int op2 = tryParseAndSubstituteVariable(operands[1].trim());
+                ignoreLines = op1 < op2;
+            } else if (argument.contains("!=")) {
+                String[] operands = argument.split("!=");
+                int op1 = tryParseAndSubstituteVariable(operands[0].trim());
+                int op2 = tryParseAndSubstituteVariable(operands[1].trim());
+                ignoreLines = op1 != op2;
+            }
+            ignoreLines = !ignoreLines;
         }
     }
 
     public int endLatestBlockCommand(int linePointer) {
         BlockCommand command = blockCommandStack.peek();
         if (!command.repeat) {
+            ignoreLines = false;
             blockCommandStack.pop();
             return linePointer;
         }
@@ -65,5 +120,46 @@ public class Runner {
         }
 
         return command.startingLine;
+    }
+
+    public void assignVariable(String variableName, String argument) {
+        Integer intValue;
+        try {
+            intValue = Integer.parseInt(argument);
+        } catch (NumberFormatException e) {
+            if (argument.contains("+")) {
+                String[] operands = argument.split("\\+");
+                int op1 = tryParseAndSubstituteVariable(operands[0].trim());
+                int op2 = tryParseAndSubstituteVariable(operands[1].trim());
+                intValue = op1 + op2;
+            } else if (argument.contains("-")) {
+                String[] operands = argument.split("-");
+                int op1 = tryParseAndSubstituteVariable(operands[0].trim());
+                int op2 = tryParseAndSubstituteVariable(operands[1].trim());
+                intValue = op1 - op2;
+            } else if (argument.contains("*")) {
+                String[] operands = argument.split("\\*");
+                int op1 = tryParseAndSubstituteVariable(operands[0].trim());
+                int op2 = tryParseAndSubstituteVariable(operands[1].trim());
+                intValue = op1 * op2;
+            } else if (argument.contains("/")) {
+                String[] operands = argument.split("/");
+                int op1 = tryParseAndSubstituteVariable(operands[0].trim());
+                int op2 = tryParseAndSubstituteVariable(operands[1].trim());
+                intValue = op1 / op2;
+            } else {
+                intValue = variableMap.get(argument);
+            }
+        }
+        variableMap.put(variableName, intValue);
+        System.out.println("Assigned to variable: " + variableName + " value: " + intValue);
+    };
+
+    private Integer tryParseAndSubstituteVariable(String expression) {
+        try {
+            return Integer.parseInt(expression);
+        } catch (NumberFormatException e) {
+            return variableMap.get(expression);
+        }
     }
 }
